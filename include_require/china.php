149374ed9a9bb368ebf9b08ec384ac6009193a62
guihensoares <guilhers53292@gmail.com>
2025-03-13 18:22:04 -0300

    alterações_13/03

--- include_require/china.php
+++ include_require/china.php
@@ -1,4 +1,38 @@
 <?php include './header.php'; ?>
     
-<h1>China</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <h1>Muralhas da China!</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img style= 'width: 100%' src="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2710344.jpg?w=1900&h=1272">
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <img style= 'width: 100%' src="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2710344.jpg?w=1900&h=1272">
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <img style= 'width: 100%' src="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2710344.jpg?w=1900&h=1272">
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <img style= 'width: 100%' src="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2710344.jpg?w=1900&h=1272">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 <?php include './footer.php'; ?>
