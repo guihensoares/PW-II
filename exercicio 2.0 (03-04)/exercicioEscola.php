30c0cde17cf6a972f71b9c053aefee0f60534ec9
guihensoares <guilhers53292@gmail.com>
2025-04-06 02:20:23 -0300

    terminando

--- exercicio 2.0 (03-04)/exercicioEscola.php
+++ exercicio 2.0 (03-04)/exercicioEscola.php
@@ -1,27 +1,23 @@
 <?php
 
+//Foi na gambiarra, mas funcionou que nem o exemplo
+
     $sobreEscola = [
         'escola' => [
             'nome' => 'Etec',
-        ],
-
-        'cursos' => [
+        
             'nomeCurso1' => 'Desenvolvimento de Sistemas' ,
-            'nomeCurso2' => 'Administração'
-        ],
-
-        'materia' => [
+            'nomeCurso2' => 'Administração',
+        
             'materia1' => 'Programação Mobile',
             'materia2' => 'Programação Web',
             'materia3' => 'Técnicas Avançadas de Programação de Algoritmos',
             'materia4' => 'Inglês',
-            'materia1' => 'Recursos Humanos',
+            'materia5' => 'Recursos Humanos',
             'materia6' => 'Gestão de Pessoas',
             'materia7' => 'Teoria da Administração',
             'materia8' => 'Inglês',
-        ],
-
-        'modulos' => [
+        
             'moduloUm' => 'Módulo I',
             'moduloDois' => 'Módulo II',
             'moduloTres' => 'Módulo III',
@@ -29,4 +25,57 @@
         ],
     ];
 
+    foreach ($sobreEscola as $key => $value) {
+        echo "<br> <li>" . $value['nome'];
+
+        foreach ($sobreEscola as $key => $value) {
+            echo "<br> <ul> <li>" . $value['nomeCurso1'];
+
+            foreach ($sobreEscola as $key => $value) {
+                echo "<br> <ul> <li>" . $value['materia1'];
+
+                foreach ($sobreEscola as $key => $value) {
+                    echo "<br> <ul> <li>" . $value['moduloUm'];
+                    echo "<br> <li>" . $value['moduloDois'] . "</ul>";
+                }
+
+                echo "<br> <li>" . $value['materia2'];
+
+                foreach ($sobreEscola as $key => $value) {
+                    echo "<br> <ul> <li>" . $value['moduloUm'];
+                    echo "<br> <li>" . $value['moduloDois'];
+                    echo "<br> <li>" . $value['moduloTres'];
+                    echo "<br> <li>" . $value['moduloQuatro'] . "</ul>";
+                }
+
+                echo "<br> <li>" . $value['materia3'];
+                echo "<br> <li>" . $value['materia4'];
+
+                foreach ($sobreEscola as $key => $value) {
+                    echo "<br> <ul> <li>" . $value['moduloUm'];
+                    echo "<br> <li>" . $value['moduloDois'];
+                    echo "<br> <li>" . $value['moduloTres'] . "</ul> </ul>";
+                }
+            }
+
+            echo "<br> <li>" . $value['nomeCurso2'];
+
+            foreach ($sobreEscola as $key => $value) {
+                echo "<br> <ul> <li>" . $value['materia5'];
+
+                foreach ($sobreEscola as $key => $value) {
+                    echo "<br> <ul> <li>" . $value['moduloUm'];
+                    echo "<br> <li>" . $value['moduloDois'] . "</ul>";
+                }
+
+                echo "<br> <li>" . $value['materia6'];
+                echo "<br> <li>" . $value['materia7'];
+                echo "<br> <li>" . $value['materia8'];
+
+                foreach ($sobreEscola as $key => $value) {
+                    echo "<br> <ul> <li>" . $value['moduloUm'] . "</ul> </ul>";
+                }
+            }
+        }
+    }
 ?>
--- exercicio 2.0 (03-04)/exercicioZoologico.php
+++ exercicio 2.0 (03-04)/exercicioZoologico.php
@@ -7,7 +7,7 @@
             'especie' => 'Dodô',
             'cor' => 'Branco e marrom',
             'peso' => '24kg',
-            'idade' =>'16',
+            'idade' =>'16anos',
             'foto' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Hoefnagel_dodo.jpg/640px-Hoefnagel_dodo.jpg',
         
         ],
@@ -17,7 +17,7 @@
             'especie' => 'Dragão',
             'cor' =>'Preto',
             'peso' => '987kg',
-            'idade' =>'67',
+            'idade' =>'67anos',
             'foto' => 'https://pm1.aminoapps.com/6311/5fb39066b693721a5ac7e277d3c5cff939459008_00.jpg',
        
         ],
@@ -27,7 +27,7 @@
             'especie' => 'cachorro',
             'cor' =>'preto',
             'peso' => '600kg',
-            'idade' =>'19',
+            'idade' =>'19anos',
             'foto' => 'https://pm1.aminoapps.com/6356/9e6c76d7cd6800a7c0c4f00918bcd1d3d08b52a6_00.jpg',
         
         ],
@@ -50,7 +50,7 @@
         echo "<br>Espécie: " . $value['especie'];
         echo "<br>Cor: " . $value['cor'];
         echo "<br>Peso: " . $value['peso'];
-        echo "<br>Idade: " . $value['idade:'];
+        echo "<br>Idade: " . $value['idade'];
         echo "<br> <img src =" . $value['foto'] . ">";
         echo "<hr>";
 
